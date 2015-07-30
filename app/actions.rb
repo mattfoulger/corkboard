7f90ad11d118a9e0bb7f1306b2293ff708364f6d
mattfoulger <mfoulger@gmail.com>
2015-07-29 19:36:30 -0700

    ajax for loading boards and creating pins. Used Masonry for grid layout

--- app/actions.rb
+++ app/actions.rb
@@ -4,4 +4,40 @@ get '/' do
 end
 
 
+get '/boards/:id' do
+  @pins = Board.find(params[:id]).pins.reverse_order
+  @pins.to_json
+end
+
+get '/boards' do
+  @boards = Board.all
+  @boards.to_json
+end
+
+post '/boards' do
+  @board = Board.new
+  @board.name = params[:name]
+  @board.description = params[:description]
+  @board.save.to_json
+end
+
+get '/pins/:id' do
+  @pin = Pin.find(params[:id])
+  @pin.to_json
+end
+
+get '/pins' do
+  @pins = Pin.all.reverse_order
+  @pins.to_json
+end
+
+post '/pins' do
+  @pin = Pin.new
+  @pin.name = params[:name]
+  @pin.description = params[:description]
+  @pin.board_id = params[:board_id]
+  @pin.url = params[:url]
+  @pin.save!
+  @pin.to_json
+end
 
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,103 @@
-$(document).ready(function() {
+$(function() {
+  var $boardsDropDown = $("#boards-dropdown");
+  var $content = $("#content");
+  var currentBoardID;
+  var boardsListData;
+  var boardData;
+  
+  var $grid = $('.corkboard').masonry({
+    itemSelector: '.pin',
+    columnWidth: 270
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function getBoards() {
+    $.get(
+      '/boards',
+      function(data) {
+        boardsListData = data;
+        boardsListData.forEach(function(board) {
+          $boardsDropDown.append(
+            $("<li>").append(
+              $("<a class='board-link' href='#'>").text(board.name).data("data", {id: board.id})
+            )
+          )
+        });
+        currentBoardID = boardsListData[0].id;
+        loadBoard(boardsListData[0].id);
+        populateNewPinForm();
+      },
+      "json"
+    );
+  }
+
+  $boardsDropDown.on('click', '.board-link', function(e){
+    e.preventDefault();
+    var id = $(this).data("data").id;
+    if (id == currentBoardID) {
+      return false;
+    }
+    loadBoard(id);
+  });
+
+  function loadBoard(id) {
+    currentBoardID = id;
+    $.get(
+      '/boards/'+id,
+      function(data) {
+        $(".corkboard").empty();
+        boardData = data;
+        boardData.forEach(function(pin) {
+          newPin(pin);
+        });
+        // var $addPin = $("<a class='pin add-pin' data-toggle='modal' data-target='#newPinModal'>");
+        // $(".corkboard").prepend($addPin);
+        $grid.masonry('reloadItems');
+        $grid.masonry();
+      },
+      "json"
+    );
+  }
+
+  function newPin(pin) {
+    var $newPin = $("<div class='pin pin-default'>");
+    $newPin.css("background-image", "url(" + pin.url + ")");
+    $newPin.append($("<h3 class='title'>").text(pin.name));
+    $newPin.append($("<p class='description'>").text(pin.description));
+    $grid.prepend($newPin).masonry( 'prepended', $newPin );
+  }
+
+  function populateNewPinForm() {
+    boardsListData.forEach(function(board) {
+      $("#newPinForm select").append($("<option>").attr("value", board.id).text(board.name));
+    });
+  }
+
+
+  $("#newPinForm").submit(function(e) {
+    e.preventDefault();
+    var formData = $("#newPinForm").serialize();
+    console.log(formData);
+    $.post(
+      '/pins',
+      formData,
+      function(data) {
+        console.log(data);
+        newPin(data);
+      },
+      "json"
+    );
+  })
+
+  $content.on('click', '.pin.pin-default', function(e) {
+    if($(this).hasClass('selected')) {
+
+    } else {
+    $('.selected').toggleClass("selected");
+    $(this).toggleClass("selected");
+    $grid.masonry();
+    console.log("it completed");
+    }
+  })
+
+  getBoards();
 });
